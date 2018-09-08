387dbe6fc241e5081725e4845d58c1a95ea28cb0
grcameron <grant.r.cameron@gmail.com>
2018-09-08 15:56:45 +0100

    added conditions to server
    added methods to obtain json objects on request.  Still to add Response Generator.

--- index.js
+++ index.js
@@ -1,9 +1,43 @@
 var http = require('http');
-http.createServer(function (req, res) { 
-    // A parsed url to work with in case there are parameters var _url; 
+var employeeService = require('./lib/employees');
+
+http.createServer(function (req, res) {
+    // A parsed url to work with in case there are parameters 
+    var _url;
     // In case the client uses lower case for methods. 
     req.method = req.method.toUpperCase();
     console.log(req.method + ' ' + req.url);
-    res.end('The current time is ' + Date.now())
+    if (req.method !== 'GET') {
+        res.writeHead(501, {
+            'Content-Type': 'text/plain'
+        });
+        return res.end(req.method + ' is not implemented by this ↵server.');
+    }
+    if (_url = /^\/employees$/i.exec(req.url)) {
+        // return a list of employees 
+        employeeService.getEmployees(function (error, data) {
+                if (error) {
+                    // send a 500 error 
+                } 
+                    // send the data with a 200 status code 
+        });
+    }
+        
+    else if (_url = /^\/employees\/(\d+)$/i.exec(req.url)) {
+        //find the id in the route 
+        employeeService.getEmployee(_url[1], function (error, data) {
+            if (error) {
+                // send a 500 error 
+            }
+            if (!data) {
+                // send a 404 error 
+            }
+            // send the data with a 200 status code 
+
+        });
+        
+    } else {
+        // try to send the static file if it exists,
+        // if not, send a 404 
+    }
 }).listen(1337, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:1337/');
